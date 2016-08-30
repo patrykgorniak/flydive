e92f5d67250c730f5b1ec6b9383e5daf4c5c42b2
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2016-08-30 11:38:58 +0200

    CfgFile: Added reading existing config Added support for database cfg

--- src/common/ConfigurationManager.py
+++ src/common/ConfigurationManager.py
@@ -2,39 +2,64 @@ import configparser
 import os
 
 class CfgMgr:
-    # __instance = None
 
-    # def __new__(cls):
-    #     if CfgMgr.__instance is None:
-    #         CfgMgr.__instance = object.__new__(cls)
-    #     return CfgMgr.__instance
+    def __init__(self):
+        self.cfg = self.Config()
+        self.__instance = self.cfg.getConfig()
 
-    # """Docstring for CfgMgr. """
+    def getConfig(self):
+        return self.__instance
 
-    def __init__(self, dirname='configs',filename='FlyDive.cfg'):
-        self.dirname = dirname
-        self.filename = filename
-        self.path = os.path.join('.', self.dirname, self.filename)
-        if not os.path.exists(self.path):
-            self.__init()
-        
-    def __init(self):
-        """TODO: Docstring for restoredefault.
-        """
-        config = configparser.ConfigParser(allow_no_value=True)
+    class Config:
 
-        config['GENERAL'] = {
-            'author_name': 'Patryk Górniak',
-            'author_mail': 'patryk.lukasz.gorniak@gmail.com',
-            'app_name': 'FlyDive',
-            'version': 0.1,
-        }
-        config['PLUGINS'] = {
-            'wizzair': 'on',
-            'ryanair': 'off'
-        }
-
-        with open(self.path,'w') as configfile:
-            config.write(configfile)
+        def __init__(self, dirname='configs',filename='FlyDive.cfg'):
+            self.__instance = None
+            self.dirname = dirname
+            self.filename = filename
+            self.path = os.path.join('.', self.dirname, self.filename)
+            if not os.path.exists(self.path):
+                self.__instance = self.__init()
+            else:
+                config = configparser.ConfigParser()
+                config.read(self.path)
+                self.__instance = config
 
 
+
+        def __init(self):
+            """TODO: Docstring for restoredefault.
+            """
+            config = configparser.ConfigParser(allow_no_value=True)
+
+            config['GENERAL'] = {
+                'author_name': 'Patryk Górniak',
+                'author_mail': 'patryk.lukasz.gorniak@gmail.com',
+                'app_name': 'FlyDive',
+                'version': 0.1,
+            }
+            config['PLUGINS'] = {
+                'wizzair': 'on',
+                'ryanair': 'off'
+            }
+
+            config['DATABASE'] = {
+                'name': 'flydive',
+                'type': 'sqlite'
+            }
+
+            with open(self.path,'w') as configfile:
+                config.write(configfile)
+
+        def getConfig(self):
+            """TODO: Docstring for getConfig.
+            :returns: TODO
+
+            """
+            return self.__instance
+
+def main():
+    mgr = CfgMgr().getConfig();
+    print(mgr.sections())
+
+if __name__ == "__main__":
+    main()
