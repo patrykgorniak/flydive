7238d1012d9c7c6231ef24c98dd99b7c11e95b91
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2016-09-02 00:06:09 +0200

    Added fetching airports and adding to db

--- flydive/wizzair/wizzairmanager.py
+++ flydive/wizzair/wizzairmanager.py
@@ -1,4 +1,8 @@
 from wizzair.wizzairdl import WizzairDl
+from common.DatabaseManager import DatabaseManager
+from common.DatabaseModel import Airport
+from common.ConfigurationManager import CfgMgr
+from common import LogManager as lm
 
 class WizzairPlugin(object):
 
@@ -6,23 +10,32 @@ class WizzairPlugin(object):
 
     def __init__(self):
         self._dlMgr = WizzairDl()
-        """TODO: to be defined1. """
-    
-    def getConnections(self):
+        cfg = CfgMgr().getConfig()
+        self.db = DatabaseManager(cfg['DATABASE']['name'], cfg['DATABASE']['type'])
+
+    def log(self, message):
+        lm.debug("WizzairPlugin: {0}".format(message))
+
+    def fetchConnections(self):
         """TODO: Docstring for getConnections.
         :returns: TODO
 
         """
-        print("getConnections");
+        self.log("fetchConnections");
         return self._dlMgr.getConnections()
 
-    def getAirports(self):
+    def fetchAirports(self):
         """TODO: Docstring for getAirports.
 
         :returns: TODO
 
         """
-        return self._dlMgr.getAirports()
+        self.log("fetchAirports");
+        for row in self._dlMgr.getAirports():
+            if row is not None:
+                airport = Airport(iata=row['IATA'], name=row['ShortName'],
+                                  latitude=row['Latitude'], longitude=row['Longitude'], country='N/A')
+                self.db.addAirport(airport)
 
     def downloadData(self):
 
