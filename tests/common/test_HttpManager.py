c91571e2320634ad12b2dfe2cbd75f0005f8c5fc
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2016-09-01 23:54:23 +0200

    Project structure refactoring

---
+++ tests/common/test_HttpManager.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestHttpManager(unittest.TestCase):
+
+    """Docstring for TestHttpManager. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+        unittest.TestCase.__init__(self)
+
+        
---
+++ tests/wizzair/test_WizzairManager.py
@@ -0,0 +1,16 @@
+from flydive.wizzair.wizzairmanager import WizzairPlugin
+import json
+import unittest
+
+
+class TestWizzairPlugin(unittest.TestCase):
+
+    """Docstring for TestWizzairDownloader. """
+    def setUp(self):
+        pass
+
+    def test_fetchConnections(self):
+        pass
+
+    def test_fetchAirports(self):
+        pass
