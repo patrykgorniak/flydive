9181821cb777fc813a8874edbb7718d1b809e458
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2016-09-02 00:00:56 +0200

    LogManager: added support for config

--- flydive/common/LogManager.py
+++ flydive/common/LogManager.py
@@ -1,11 +1,24 @@
 import logging
 import logging.config
 import os.path
+from common.ConfigurationManager import CfgMgr
+
+logHandler=None
 
 def init(dirname='configs', configFileName="logger.cfg"):
     _configFileName = configFileName
     _dirname = dirname
 
+    global logHandler
     logging.config.fileConfig(os.path.join('.', _dirname, _configFileName))
-    logger = logging.getLogger('default')
-    logger.debug("Logger init.")
+    logHandler = logging.getLogger('default')
+
+def debug(log):
+    """TODO: Docstring for getLogger.
+
+    :module: TODO
+    :returns: TODO
+
+    """
+    if CfgMgr().getConfig()['LOGGER']['debug'] == 'on':
+        logHandler.debug(log)
