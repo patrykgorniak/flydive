91f3f53fa274874ff0ca0f5d4c3880dfe2988196
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2016-09-06 23:24:17 +0200

    Main: Minor refactoring

--- flydive/main.py
+++ flydive/main.py
@@ -10,20 +10,13 @@ from common import LogManager as LogMgr
 
 def setupLogger(cfg):
     LogMgr.init(dirname=cfg['config_dir'], configFileName=cfg['config_name'])
-    # LogMgr.init()
-
 
 def main():
     cfg = CfgMgr().getConfig()
     setupLogger(cfg['LOGGER'])
 
     wiz = WizzairPlugin();
-    wiz.fetchAirports()
-   # printJSON(wiz.fetchConnections())
-
-
-    # httpcontent = HttpManager.getPage(CommonData.AIRPORTS.value)
-    # print(marketInfo.getAirports(httpcontent)[0]['ShortName'])
+    wiz.run()
 
 if __name__ == "__main__":
     main()
