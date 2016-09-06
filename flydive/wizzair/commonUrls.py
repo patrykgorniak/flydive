061652b4487f48346aaf8b02f378deb6b7d0ca4b
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2016-09-06 23:19:00 +0200

    Wizzair: added flight details URL

--- flydive/wizzair/commonUrls.py
+++ flydive/wizzair/commonUrls.py
@@ -2,4 +2,5 @@ from enum import Enum
 
 class CommonData(Enum):
     AIRPORTS = "https://cdn.static.wizzair.com/pl-PL/Markets.js"
+    TimeTable = "https://cdn.static.wizzair.com/pl-PL/TimeTableAjax?departureIATA={0}&arrivalIATA={1}&year={2}&month={3}"
 
