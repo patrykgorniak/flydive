8b3e07631d7087cc8d58d5dbd6ebffc3d74bb996
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2016-10-15 17:29:33 +0200

    CommonURLs: Search flight (wizzairApi_3.0) URL

--- flydive/wizzair/commonUrls.py
+++ flydive/wizzair/commonUrls.py
@@ -3,4 +3,4 @@ from enum import Enum
 class CommonData(Enum):
     AIRPORTS = "https://cdn.static.wizzair.com/pl-PL/Markets.js"
     TimeTable = "https://cdn.static.wizzair.com/pl-PL/TimeTableAjax?departureIATA={0}&arrivalIATA={1}&year={2}&month={3}"
-
+    Search = "https://be.wizzair.com/3.2.0/Api/search/search"
