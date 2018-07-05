1312b916267004869a151844452b3baa95a38ee6
Patryk Górniak <patryk.lukasz.gorniak@gmail.com>
2018-07-05 21:50:38 +0200

    RyanairDl: changed fetching data from server.

--- flydive/plugins/ryanair/RyanairDl.py
+++ flydive/plugins/ryanair/RyanairDl.py
@@ -1,5 +1,5 @@
 import json
-import datetime
+import datetime, time
 from plugins.ryanair import RyanairUrls as RyanairData
 from common import HttpManager as HttpMgr
 
@@ -8,7 +8,7 @@ class RyanairDl():
     """Docstring for RyanairDl. """
 
     def __init__(self):
-        self.apikey = "YourApiKey"
+        self.apikey = "gCUKLGIa2t94BtZrpIdHAmW7p7Tac5qH"
         self.s_iata = ""
         self.d_iata = ""
         self.date = datetime.date.min
@@ -16,13 +16,13 @@ class RyanairDl():
 
     def getAirports(self):
         url = self.__attachCredentials(RyanairData.CommonData.AIRPORTS)
-        airports_text = HttpMgr.getMethod(url, tries = 10).text
+        airports_text = self.getData(url) #HttpMgr.getMethod(url, tries = 10).text
         json_data = json.loads(airports_text)
         return json_data
 
     def getConnections(self):
         url = self.__attachCredentials(RyanairData.CommonData.CONNECTIONS)
-        connections_text = HttpMgr.getMethod(url, tries = 10).text
+        connections_text = self.getData(url) #HttpMgr.getMethod(url, tries = 10).text
         json_data = json.loads(connections_text)
         return json_data
 
@@ -41,8 +41,8 @@ class RyanairDl():
         url_from = url.format(src_iata, dst_iata )
         url_back = url.format(dst_iata, src_iata )
 
-        httpContent_from = HttpMgr.getMethod(url_from, tries = 10).text
-        httpContent_to = HttpMgr.getMethod(url_back, tries = 10).text
+        httpContent_from = self.getData(url_from) #HttpMgr.getMethod(url_from, tries = 10).text
+        httpContent_to = self.getData(url_back) #HttpMgr.getMethod(url_back, tries = 10).text
 
         self.timetable  = { "from": json.loads(httpContent_from), "to": json.loads(httpContent_to) }
         
@@ -67,8 +67,19 @@ class RyanairDl():
         if self.sig == [s_iata, d_iata, date.year, date.month]:
             return self.flightDetails
         
-        self.flightDetails = HttpMgr.getMethod(url)
+        self.flightDetails = self.getData(url) #HttpMgr.getMethod(url)
         self.sig = [s_iata, d_iata, date.year, date.month]
         
         return self.flightDetails
 
+    def getData(self, url):
+        tries = 10
+        counter = 0
+        while True:
+            r = HttpMgr.getMethod(url, tries = 0)
+            if (r != None):
+                return r.text;
+            counter += 1
+            time.sleep( counter * 2)
+            if counter > tries:
+                break
